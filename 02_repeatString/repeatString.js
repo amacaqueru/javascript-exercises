ab159f2d09338651112655656a842bc4b29a772e
amacaqueru <nmreinosoc@gmail.com>
2025-04-29 15:19:55 -0500

    actualizacion archivos

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,27 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+
+    if (num < 0) {
+        return "ERROR";
+    }
+
+    let i = 0;
+    let frase= "";
+
+    while (i < num){
+        frase = frase + string;
+        console.log(frase);
+
+        i++;
+    }
+    return frase;
+
 
 };
 
 // Do not edit below this line
 module.exports = repeatString;
+
+///////////////////////////////////////////////////////
+
+
+
