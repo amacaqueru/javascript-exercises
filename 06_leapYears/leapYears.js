6ca1b86d1bf64a89c09e313dab5b4b25950de22e
amacaqueru <nmreinosoc@gmail.com>
2025-04-30 16:11:42 -0500

    leap years working

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,27 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
-};
+
+        if (year % 100 == 0 && year % 400 == 0 && year % 4 ==0){
+            return true;
+        }else if (year %4 == 0 && year%100 != 0 && year %400 != 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+
+
+        // console.log("yes");
+        // return true;
+
+    };
 
 // Do not edit below this line
 module.exports = leapYears;
+
+
+
+////////////////////////////
+
+
+// leapYears(1984);
