8241da8446f010aa29d3ddf40ee95cb2ed6bd6a0
amacaqueru <nmreinosoc@gmail.com>
2025-04-30 16:37:00 -0500

    finished temperature

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let newCel = parseFloat(((fahrenheit - 32) * 5/9).toFixed(1));
+  return newCel;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let newFar = parseFloat(((celsius * 9/5) + 32).toFixed(1));
+  return newFar;
 };
 
 // Do not edit below this line
@@ -9,3 +13,7 @@ module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+///////////////////////////////////////////
+
+// console.log(convertToCelsius(100));
+
