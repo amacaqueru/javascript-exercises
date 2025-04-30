99eca00b3dc81c9544ddb6088e5cc00bb68fb414
amacaqueru <nmreinosoc@gmail.com>
2025-04-30 14:31:38 -0500

    sum all finished

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,30 @@
-const sumAll = function() {
+const sumAll = function(first, last) {
 
+    if (Number.isInteger(first) && Number.isInteger(last) && first >= 0 && last >=0){
+        let order = 0;
+
+        if(first > last){
+            order = last;
+            last = first;
+            first = order;
+        }
+
+        let sum = 0;
+        for(let i = first; i<= last; i++){
+            sum = sum +i;
+            console.log(sum);
+        }
+        return sum;
+        }
+        else{
+            return "ERROR";
+        }
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
+
+
+
+// sumAll(1,4);
